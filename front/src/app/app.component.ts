c04c7ea200afa489a01b5230454bd0bd4b6600fc
Adrian Ruiz <adrianruiz0208@gmail.com>
2025-02-14 23:55:19 -0500

    Avance en la interfaz de Mancala

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, signal, Signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [CommonModule]
 })
 export class AppComponent {
-  title = 'front';
+  player1Pits: Signal<number[]> = signal(Array(6).fill(4)); // Casas jugador 1
+  player2Pits: Signal<number[]> = signal(Array(6).fill(4)); // Casas jugador 2
+  stores: Signal<number[]> = signal([0, 0]); // Almacenes de cada jugador
+
+  moveStones(index: number, player: number): void {
+    console.log(`Jugador ${player} movió las piedras de la casa ${index}`);
+    // Implementa la lógica de movimiento aquí.
+  }
+
+  play(): void {
+    console.log('Iniciando juego');
+    // Implementa la lógica de juego aquí.
+  }
+
+  reset(): void {
+    console.log('Reiniciando juego');
+    // Implementa la lógica de reinicio aquí.
+  }
 }
